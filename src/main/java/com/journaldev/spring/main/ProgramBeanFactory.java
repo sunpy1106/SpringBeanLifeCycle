a381d56adcfe50eb5212a85892e161852bcde6db
sunpy1106 <sunpy1106@qq.com>
2016-08-21 08:20:04 +0800

    replace documentReader.processBeanDefinition

--- src/main/java/com/journaldev/spring/main/ProgramBeanFactory.java
+++ src/main/java/com/journaldev/spring/main/ProgramBeanFactory.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.springframework.beans.factory.BeanDefinitionStoreException;
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
+import org.springframework.beans.factory.parsing.BeanComponentDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionReaderUtils;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.BeanDefinitionDocumentReader;
 import org.springframework.beans.factory.xml.BeanDefinitionParserDelegate;
@@ -27,6 +30,7 @@ public class ProgramBeanFactory {
 		DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
 		System.out.println("create xml file reader ");
 		XmlBeanDefinitionReader reader  = new XmlBeanDefinitionReader(factory);
+		int countBefore = reader.getRegistry().getBeanDefinitionCount();
 		System.out.println("use the xmlBeanDefinitionReader to load the resource");
 		EncodedResource encodedResource = new EncodedResource(resource);
 		try {
@@ -42,28 +46,37 @@ public class ProgramBeanFactory {
 					//reader.registerBeanDefinitions(doc, resource);
 					DefaultBeanDefinitionDocumentReader documentReader = (DefaultBeanDefinitionDocumentReader)reader.createBeanDefinitionDocumentReader();
 					documentReader.setEnvironment(reader.getEnvironment());
-					int countBefore = reader.getRegistry().getBeanDefinitionCount();
 					//core code
 					//documentReader.registerBeanDefinitions(doc, reader.createReaderContext(resource));
 					documentReader.setReaderContext(reader.createReaderContext(resource));
 					Element root = doc.getDocumentElement();
 					//documentReader.doRegisterBeanDefinitions(root);
 					BeanDefinitionParserDelegate parent = documentReader.delegate;
-					documentReader.delegate = documentReader.createDelegate(documentReader.readerContext, root, parent);
-					//documentReader.parseBeanDefinitions(root, documentReader.delegate);
-					BeanDefinitionParserDelegate curDelegate = documentReader.delegate;
-					if (curDelegate.isDefaultNamespace(root)) {
-						
+					BeanDefinitionParserDelegate curDelegate  = documentReader.createDelegate(documentReader.readerContext, root, parent);
+					//documentReader.parseBeanDefinitions(root, documentReader.delegate);				
+					if (curDelegate.isDefaultNamespace(root)) {			
 						NodeList nl = root.getChildNodes();
 						for (int i = 0; i < nl.getLength(); i++) {
 							Node node = nl.item(i);
-							System.out.println(node.getBaseURI()+":" + node.getNodeName()+":"+node.getNodeValue());
 							if (node instanceof Element) {
 								Element ele = (Element) node;
 								if (curDelegate.isDefaultNamespace(ele)) {
 									//documentReader.parseDefaultElement(ele, curDelegate);
 									if (curDelegate.nodeNameEquals(ele, BeanDefinitionParserDelegate.BEAN_ELEMENT)) {
-										documentReader.processBeanDefinition(ele, curDelegate);
+										//documentReader.processBeanDefinition(ele, curDelegate);
+										BeanDefinitionHolder bdHolder = curDelegate.parseBeanDefinitionElement(ele,null);
+										if (bdHolder != null) {
+											bdHolder = curDelegate.decorateBeanDefinitionIfRequired(ele, bdHolder);
+											try {
+												// Register the final decorated instance.
+												BeanDefinitionReaderUtils.registerBeanDefinition(bdHolder, documentReader.getReaderContext().getRegistry());
+											}
+											catch (BeanDefinitionStoreException ex) {
+												documentReader.getReaderContext().error("Failed to register bean definition with name '" +
+														bdHolder.getBeanName() + "'", ele, ex);
+											}
+
+										}
 									}
 								}
 							}
@@ -85,8 +98,7 @@ public class ProgramBeanFactory {
 			throw new BeanDefinitionStoreException(
 					"IOException parsing XML document from " + encodedResource.getResource(), ex);
 		}
-		//reader.loadBeanDefinitions(resource);
-		
+
 
 		System.out.println("get bean");
 		MyAwareService service = factory.getBean("myAwareService",MyAwareService.class);
